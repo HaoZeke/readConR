222c0be9bb1b5b24c3c967514912ecfde24d98bc
Rohit Goswami <rog32@hi.is>
2023-07-23 04:45:16 +0000

    MAINT: Silence styler error

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,28 +6,26 @@
 using namespace Rcpp;
 
 #ifdef RCPP_USE_GLOBAL_ROSTREAM
-Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
-Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+Rcpp::Rostream<true> &Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false> &Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 #endif
 
 // readCon
 Rcpp::List readCon(std::string filename);
 RcppExport SEXP _readConR_readCon(SEXP filenameSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< std::string >::type filename(filenameSEXP);
-    rcpp_result_gen = Rcpp::wrap(readCon(filename));
-    return rcpp_result_gen;
-END_RCPP
+  BEGIN_RCPP
+  Rcpp::RObject rcpp_result_gen;
+  Rcpp::RNGScope rcpp_rngScope_gen;
+  Rcpp::traits::input_parameter<std::string>::type filename(filenameSEXP);
+  rcpp_result_gen = Rcpp::wrap(readCon(filename));
+  return rcpp_result_gen;
+  END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_readConR_readCon", (DL_FUNC) &_readConR_readCon, 1},
-    {NULL, NULL, 0}
-};
+    {"_readConR_readCon", (DL_FUNC)&_readConR_readCon, 1}, {NULL, NULL, 0}};
 
 RcppExport void R_init_readConR(DllInfo *dll) {
-    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
-    R_useDynamicSymbols(dll, FALSE);
+  R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+  R_useDynamicSymbols(dll, FALSE);
 }
