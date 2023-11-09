8f3fbd4b47ee15dade29fb29aeb8c6d55407b5d6
Rohit Goswami <rog32@hi.is>
2023-11-09 03:23:56 +0000

    MAINT: More descriptive name

--- src/rcpp_readcon.cpp
+++ src/rcpp_readcon.cpp
@@ -9,27 +9,31 @@
 Rcpp::List readCon(std::string filename) {
   std::vector<std::string> fconts =
       yodecon::helpers::file::read_con_file(filename);
-  auto tmp = yodecon::create_single_con<yodecon::types::ConFrameVec>(fconts);
-  auto atmNumVector = yodecon::symbols_to_atomic_numbers(tmp.symbol);
+  auto singleCon =
+      yodecon::create_single_con<yodecon::types::ConFrameVec>(fconts);
+  auto atmNumVector = yodecon::symbols_to_atomic_numbers(singleCon.symbol);
   // Convert atmNumVector to IntegerVector
   Rcpp::IntegerVector atmNumVectorRcpp = Rcpp::wrap(atmNumVector);
 
   // Create a DataFrame with the vectors
   Rcpp::DataFrame df = Rcpp::DataFrame::create(
-      Rcpp::_["symbol"] = tmp.symbol, Rcpp::_["atmNum"] = atmNumVectorRcpp,
-      Rcpp::_["x"] = tmp.x, Rcpp::_["y"] = tmp.y, Rcpp::_["z"] = tmp.z,
-      Rcpp::_["is_fixed"] = tmp.is_fixed, Rcpp::_["atom_id"] = tmp.atom_id,
+      Rcpp::_["symbol"] = singleCon.symbol,
+      Rcpp::_["atmNum"] = atmNumVectorRcpp, Rcpp::_["x"] = singleCon.x,
+      Rcpp::_["y"] = singleCon.y, Rcpp::_["z"] = singleCon.z,
+      Rcpp::_["is_fixed"] = singleCon.is_fixed,
+      Rcpp::_["atom_id"] = singleCon.atom_id,
       Rcpp::_["stringsAsFactors"] = false);
 
   Rcpp::List conFrame = Rcpp::List::create(
       Rcpp::Named("prebox_header") =
-          yodecon::helpers::string::to_csv_string(tmp.prebox_header),
-      Rcpp::Named("boxl") = tmp.boxl, Rcpp::Named("angles") = tmp.angles,
+          yodecon::helpers::string::to_csv_string(singleCon.prebox_header),
+      Rcpp::Named("boxl") = singleCon.boxl,
+      Rcpp::Named("angles") = singleCon.angles,
       Rcpp::Named("postbox_header") =
-          yodecon::helpers::string::to_csv_string(tmp.postbox_header),
-      Rcpp::Named("natm_types") = tmp.natm_types,
-      Rcpp::Named("natms_per_type") = tmp.natms_per_type,
-      Rcpp::Named("masses_per_type") = tmp.masses_per_type,
+          yodecon::helpers::string::to_csv_string(singleCon.postbox_header),
+      Rcpp::Named("natm_types") = singleCon.natm_types,
+      Rcpp::Named("natms_per_type") = singleCon.natms_per_type,
+      Rcpp::Named("masses_per_type") = singleCon.masses_per_type,
       Rcpp::Named("atom_data") = df);
   conFrame.attr("class") = "conFrame";
   return conFrame;
